785d81ae999269b4a7e42beb5489b4f02b985d19
Dževada Džafić <dzevada93@gmail.com>
2015-01-13 14:21:49 +0100

    popravljeni grafi

--- slike/grafi.r
+++ slike/grafi.r
@@ -2,13 +2,22 @@ cat("Uvažam grafe...\n")
 
 pdf("slike/grafi1.pdf", paper="a4r")
 
-barplot(trajanjebrez[1:12,9], xlab="statistična regija", ylab="trajanje bezposelnosti", main="Trajanje brezposlenosti po statističnih regijah v Sloveniji v letu 2014", col="lightblue", names.arg = trajanjebrez$STATISTI.NA.REGIJA, las = 2, cex.names=0.5)
+regije <- trajanjebrez$STATISTI.NA.REGIJA
+regije[1] <- "Jugovzhodna\nSlovenija"
+barplot(trajanjebrez[1:12,9], sub="statistična regija", ylab="trajanje bezposelnosti",
+        main="Trajanje brezposlenosti po statističnih regijah v Sloveniji v letu 2014",
+        col="lightblue", names.arg = regije, las = 2, cex.names=0.5, cex.axis=0.7)
 
 dev.off()
 
 pdf("slike/grafi2.pdf", paper="a4r")
 
-barplot(brezstarost[1:12,7], xlab="statistična regija", ylab="brezposelnost prebivalcev starejših od 55 let", main="Brezposelnost prebivalcev starejših od 55 let v letu 2014", col="pink", names.arg = brezstarost$STATISTIČNA.REGIJA, las = 2, cex.names=0.5)
+regije <- trajanjebrez$STATISTI.NA.REGIJA
+regije[1] <- "Jugovzhodna\nSlovenija"
+barplot(brezstarost[1:12,7], sub="statistična regija", ylab="brezposelnost prebivalcev starejših od 55 let",
+        main="Brezposelnost prebivalcev starejših od 55 let v letu 2014",
+        col="pink", names.arg = regije, las = 2, cex.names=0.5, cex.axis=0.7)
+
 
 
 dev.off()
